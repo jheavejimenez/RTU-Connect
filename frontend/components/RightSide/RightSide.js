b891d166413858d72dfb10ae9a725ba957a4aef9
jheave <jimenezjheave123@gmail.com>
2022-09-02 19:27:34 +0800

    added rightside component

---
+++ frontend/components/RightSide/RightSide.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import "./RightSide.css";
+import TrendCard from "../TrendCard/TrendCard";
+import ShareModal from "../ShareModal/ShareModal";
+
+
+function RightSide() {
+    const [modalOpened, setModalOpened] = useState(false);
+    return (
+        <div className="RightSide">
+            <div className="navIcons">
+                <img src="" alt=""/>
+                <UilSetting/>
+                <img src="" alt=""/>
+                <img src="" alt=""/>
+            </div>
+
+            <TrendCard/>
+
+            <button className="button r-button" onClick={() => setModalOpened(true)}>
+                Share
+            </button>
+            <ShareModal modalOpened={modalOpened} setModalOpened={setModalOpened}/>
+        </div>
+    );
+}
+
+export default RightSide;
