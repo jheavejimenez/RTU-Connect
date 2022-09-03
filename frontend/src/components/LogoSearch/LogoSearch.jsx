740f5f477afd6ecb38d3daee9c51dc052e4a12bd
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-09-03 19:54:09 +0800

    added post

--- frontend/src/components/LogoSearch/LogoSearch.jsx
+++ frontend/src/components/LogoSearch/LogoSearch.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 function LogoSearch() {
   return (
-   {/*<div className="LogoSearch">
-       <img src={Logo} alt="" />
+   <div className="LogoSearch">
+       {/*<img src={Logo} alt="" />
        <div className="Search">
            <input type="text" placeholder='#Explore' />
            <div className="s-icon">
                <UilSearch/>
            </div>
-       </div>
-   </div>*/}
+       </div>*/}
+   </div>
   )
 }
 
---
+++ frontend/src/components/Post/Post.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Post() {
+  return (
+      <div className="Post">
+     {/*   <img src={data.img} alt="" />
+
+
+        <div className="postReact">
+            <img src={data.liked?Heart: NotLike} alt="" />
+            <img src={Comment} alt="" />
+            <img src={Share} alt="" />
+        </div>
+
+
+        <span style={{color: "var(--gray)", fontSize: '12px'}}>{data.likes} likes</span>
+
+        <div className="detail">
+            <span><b>{data.name}</b></span>
+            <span> {data.desc}</span>
+        </div> */}
+    </div>
+  )
+}
+
+export default Post
