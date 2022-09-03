56a17bc33ab7883ede4d9806644bce695d253e8c
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-09-03 19:44:01 +0800

    added Logo Search

--- frontend/src/components/InfoCard/InfoCard.jsx
+++ frontend/src/components/InfoCard/InfoCard.jsx
@@ -5,44 +5,44 @@ function InfoCard() {
   const [modalOpened, setModalOpened] = useState(false);
   return (
      {/*  <div className="InfoCard">
-    //   <div className="infoHead">
-    //     <h4>Your Info</h4>
-    //     <div>
-    //       <UilPen
-    //         width="2rem"
-    //         height="1.2rem"
-    //         onClick={() => setModalOpened(true)}
-    //       />
-    //       <ProfileModal
-    //         modalOpened={modalOpened}
-    //         setModalOpened={setModalOpened}
-    //       />
-    //     </div>
-    //   </div>
+      <div className="infoHead">
+        <h4>Your Info</h4>
+        <div>
+          <UilPen
+            width="2rem"
+            height="1.2rem"
+            onClick={() => setModalOpened(true)}
+          />
+          <ProfileModal
+            modalOpened={modalOpened}
+            setModalOpened={setModalOpened}
+          />
+        </div>
+      </div>
 
-    //   <div className="info">
-    //     <span>
-    //       <b>Status </b>
-    //     </span>
-    //     <span>in Relationship</span>
-    //   </div>
+      <div className="info">
+        <span>
+          <b>Status </b>
+        </span>
+        <span>in Relationship</span>
+      </div>
 
-    //   <div className="info">
-    //     <span>
-    //       <b>Lives in </b>
-    //     </span>
-    //     <span>Multan</span>
-    //   </div>
+      <div className="info">
+        <span>
+          <b>Lives in </b>
+        </span>
+        <span>Multan</span>
+      </div>
 
-    //   <div className="info">
-    //     <span>
-    //       <b>Works at </b>
-    //     </span>
-    //     <span>Zainkeepscode inst</span>
-    //   </div>
+      <div className="info">
+        <span>
+          <b>Works at </b>
+        </span>
+        <span>Zainkeepscode inst</span>
+      </div>
 
-    //   <button className="button logout-button">Logout</button>
-    // </div>*/}
+      <button className="button logout-button">Logout</button>
+    </div>*/}
   ); 
 }; 
 
