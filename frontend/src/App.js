2d3f2ae71f333db92e6643773a34340ba71d2c44
jheave <jimenezjheave123@gmail.com>
2022-09-03 17:31:51 +0800

    add tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,9 @@ import "./index.css"
 
 function App() {
   return (
-    <div className="App">
-     <Home />
-    </div>
+      <h1 className="text-3xl font-bold underline">
+          Hello world!
+      </h1>
   );
 }
 
