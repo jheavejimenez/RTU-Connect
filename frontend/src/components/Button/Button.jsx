60cc744dd86fa38e0f32f7b5b85c49caf06d8122
jheave <jimenezjheave123@gmail.com>
2022-09-09 23:09:25 +0800

    refractor

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -5,9 +5,8 @@ function Button({text}) {
         <>
             <button
                 type={"submit"}
-                className={
-                    "inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug rounded-lg hover:bg-blue-700 " +
-                    "focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                className={"w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full " +
+                    "hover:bg-blue-700 focus:outline-none focus:shadow-outline"
                 }
             >
                 {text}
