a30a9296112510670e37c493b207e4c634139d6b
jheave <jimenezjheave123@gmail.com>
2022-09-09 20:26:09 +0800

    add button component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import Home from "./pages/Home"
 import "./index.css"
+import Button from "./components/Button/Button";
 
 function App() {
   return (
       <div>
-          <Home />
+          <Button text={"Post"}/>
       </div>
   );
 }
--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
-function Button() {
+function Button({text}) {
     return (
-        <div className="Button">
-            <button className="button">Button</button>
-        </div>
+        <>
+            <button
+                type={"submit"}
+                className={
+                    "inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug rounded-lg hover:bg-blue-700 " +
+                    "focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                }
+            >
+                {text}
+            </button>
+        </>
     );
 }
 
