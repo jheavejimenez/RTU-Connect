7a95a3d50f50be0c972387cad88800bb155f47bb
jheave <jimenezjheave123@gmail.com>
2022-09-09 23:10:39 +0800

    refractor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import Home from "./pages/Home"
 import "./index.css"
 import Login from "./pages/Login"
+
 function App() {
-  return (
-    // <Home/>
-    <Login/>
-  );
+    return (
+        // <Home/>
+        <Login/>
+    );
 }
 
 export default App;
