4b7d58e580a5ce6b8312d1dcbc96331c5e80e81e
jheave <jimenezjheave123@gmail.com>
2022-09-12 18:53:02 +0800

    fix navbar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import Home from '../../icons/home-icon.png'
-import Message from '../../icons/message-icon.png'
-import Video from '../../icons/video-icon.png'
 import Logo from '../../icons/rtu-icon.png'
 import Profile from '../../icons/profile-icon.png'
 
@@ -9,31 +6,55 @@ function NavBar() {
     return (
         <div className={"NavBar"}>
             <nav className={"sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md"}>
-                <div className="flex justify-center flex-grow">
-                    <a href="https://flowbite.com/" className="flex items-center">
-                        <img src={Logo} width={40} height={40} style={{ layout: "fixed" }} />
-                    </a>
-                    <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
-                    <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-                            <input className="hidden md:inline-flex flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" placeholder="Search Facebook"/>
-                    </div>
-                    <div className="flex justify-center flex-grow">
-                        <div className="flex space-x-6 md:space-x-2">
-                            <section className="flex items-center cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-xl active:border-b-2 active:border-blue-500 group">
-                                <img src={Home} width={22} height={22} className="h-5 text-gray-500 text-center sm:h-7 mx:auto group-hover:text-blue-500 text-blue-300" />
-                            </section>
-                            <section className="flex items-center cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-xl active:border-b-2 active:border-blue-500 group">
-                                <img src={Message}   width={22} height={22}  />
-                            </section>
-                            <section className="flex items-center cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-xl active:border-b-2 active:border-blue-500 group">
-                                <img src={Video} width={22} height={22} />
-                            </section>
-
-
+                <div className={"flex justify-between mx-10 items-center w-full"}>
+                    <div className={"flex items-center"}>
+                        <a href="https://flowbite.com/" className={"mr-3"}>
+                            <img src={Logo} width={40} height={40} style={{layout: "fixed"}}/>
+                        </a>
+                        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+                            <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                      clip-rule="evenodd"></path>
+                            </svg>
+                            <input
+                                className={"hidden md:inline-flex flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"}
+                                placeholder="Search Facebook"
+                            />
                         </div>
                     </div>
+                    <ul className={"flex justify-center items-center"}>
+                        <li className={"mx-4"}>
+                            <span className="om3e55n1">
+                                <svg viewBox="0 0 28 28" fill="currentColor" height="28" width="28">
+                                    <path
+                                        d="M8.75 25.25C8.336 25.25 8 24.914 8 24.5 8 24.086 8.336 23.75 8.75 23.75L19.25 23.75C19.664 23.75 20 24.086 20 24.5 20 24.914 19.664 25.25 19.25 25.25L8.75 25.25ZM17.163 12.846 12.055 15.923C11.591 16.202 11 15.869 11 15.327L11 9.172C11 8.631 11.591 8.297 12.055 8.576L17.163 11.654C17.612 11.924 17.612 12.575 17.163 12.846ZM21.75 20.25C22.992 20.25 24 19.242 24 18L24 6.5C24 5.258 22.992 4.25 21.75 4.25L6.25 4.25C5.008 4.25 4 5.258 4 6.5L4 18C4 19.242 5.008 20.25 6.25 20.25L21.75 20.25ZM21.75 21.75 6.25 21.75C4.179 21.75 2.5 20.071 2.5 18L2.5 6.5C2.5 4.429 4.179 2.75 6.25 2.75L21.75 2.75C23.821 2.75 25.5 4.429 25.5 6.5L25.5 18C25.5 20.071 23.821 21.75 21.75 21.75Z"></path>
+                                </svg>
+                                <span className="s8sjc6am o3m1n7fs ejru8ifv"></span>
+                            </span>
+                        </li>
+                        <li className={"mx-4"}>
+                            <span className="om3e55n1">
+                                <svg viewBox="0 0 28 28" fill="currentColor" height="28" width="28">
+                                    <path
+                                        d="M8.75 25.25C8.336 25.25 8 24.914 8 24.5 8 24.086 8.336 23.75 8.75 23.75L19.25 23.75C19.664 23.75 20 24.086 20 24.5 20 24.914 19.664 25.25 19.25 25.25L8.75 25.25ZM17.163 12.846 12.055 15.923C11.591 16.202 11 15.869 11 15.327L11 9.172C11 8.631 11.591 8.297 12.055 8.576L17.163 11.654C17.612 11.924 17.612 12.575 17.163 12.846ZM21.75 20.25C22.992 20.25 24 19.242 24 18L24 6.5C24 5.258 22.992 4.25 21.75 4.25L6.25 4.25C5.008 4.25 4 5.258 4 6.5L4 18C4 19.242 5.008 20.25 6.25 20.25L21.75 20.25ZM21.75 21.75 6.25 21.75C4.179 21.75 2.5 20.071 2.5 18L2.5 6.5C2.5 4.429 4.179 2.75 6.25 2.75L21.75 2.75C23.821 2.75 25.5 4.429 25.5 6.5L25.5 18C25.5 20.071 23.821 21.75 21.75 21.75Z"></path>
+                                </svg>
+                                <span className="s8sjc6am o3m1n7fs ejru8ifv"></span>
+                            </span>
+                        </li>
+                        <li className={"mx-4"}>
+                            <span className="om3e55n1">
+                                <svg viewBox="0 0 28 28" fill="currentColor" height="28" width="28">
+                                    <path
+                                        d="M8.75 25.25C8.336 25.25 8 24.914 8 24.5 8 24.086 8.336 23.75 8.75 23.75L19.25 23.75C19.664 23.75 20 24.086 20 24.5 20 24.914 19.664 25.25 19.25 25.25L8.75 25.25ZM17.163 12.846 12.055 15.923C11.591 16.202 11 15.869 11 15.327L11 9.172C11 8.631 11.591 8.297 12.055 8.576L17.163 11.654C17.612 11.924 17.612 12.575 17.163 12.846ZM21.75 20.25C22.992 20.25 24 19.242 24 18L24 6.5C24 5.258 22.992 4.25 21.75 4.25L6.25 4.25C5.008 4.25 4 5.258 4 6.5L4 18C4 19.242 5.008 20.25 6.25 20.25L21.75 20.25ZM21.75 21.75 6.25 21.75C4.179 21.75 2.5 20.071 2.5 18L2.5 6.5C2.5 4.429 4.179 2.75 6.25 2.75L21.75 2.75C23.821 2.75 25.5 4.429 25.5 6.5L25.5 18C25.5 20.071 23.821 21.75 21.75 21.75Z"></path>
+                                </svg>
+                                <span className="s8sjc6am o3m1n7fs ejru8ifv"></span>
+                            </span>
+                        </li>
+                    </ul>
                     <div className="flex items-center sm:space-2 justify-end">
-                        <img src={Profile} width={40} height={40}  style={{ layout: "fixed" }} />
+                        <img src={Profile} width={40} height={40} style={{layout: "fixed"}}/>
                     </div>
 
 
