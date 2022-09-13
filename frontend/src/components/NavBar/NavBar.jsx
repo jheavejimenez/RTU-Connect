ec8f09e3206eba679352672e15f75aa9c1c5db69
jheave <jimenezjheave123@gmail.com>
2022-09-13 22:22:01 +0800

    refractor

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -3,6 +3,8 @@ import Logo from '../../icons/rtu-icon.png'
 import Profile from '../../icons/profile-icon.png'
 import SVGHome from "../../svg/Home";
 import SVGCommunity from "../../svg/Community";
+import Watch from "../../svg/Watch";
+import Search from "../../svg/Search";
 
 function NavBar() {
     return (
@@ -15,11 +17,7 @@ function NavBar() {
                         </a>
                         <div className={"flex ml-2 items-center rounded-full bg-gray-100 p-2"}>
                             <button className={"outline-none focus:outline-none"}>
-                                <svg className={"w-5 text-gray-600 h-5 cursor-pointer"} fill="none"
-                                     stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                     stroke="currentColor" viewBox="0 0 24 24">
-                                    <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                                </svg>
+                                <Search />
                             </button>
                             <input
                                 className={"w-full pl-3 text-sm text-black outline-none focus:outline-none bg-transparent"}
@@ -35,21 +33,19 @@ function NavBar() {
                             </span>
                         </li>
                         <li className={"mx-4"}>
-                            <span className="om3e55n1">
+                            <span>
                                 <SVGCommunity />
                             </span>
                         </li>
                         <li className={"mx-4"}>
-                            <span className="om3e55n1">
-
+                            <span>
+                                <Watch />
                             </span>
                         </li>
                     </ul>
                     <div className={"flex items-center sm:space-2 justify-end"}>
                         <img src={Profile} width={40} height={40} style={{layout: "fixed"}}/>
                     </div>
-
-
                 </div>
             </nav>
         </div>
