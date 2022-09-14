b7bff51860f82fe08857e19c965dc3cada4d1f42
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-09-14 18:50:58 +0800

    added Compose Post

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "../index.css"
 import NavBar from "../components/NavBar/NavBar";
+import ComposePost from "../components/ComposePost/ComposePost";
 
 function Home() {
     return (
         <div className="Home">
             <NavBar/>
+            <ComposePost/>
         </div>
     )
 }
