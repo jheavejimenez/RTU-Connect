eddb13c7f057bc689ff360fe13c71ee5d4fe79f2
jheave <jimenezjheave123@gmail.com>
2022-09-16 22:19:29 +0800

    refractor

--- frontend/src/utils/PrivateRoutes.jsx
+++ frontend/src/utils/PrivateRoutes.jsx
@@ -3,7 +3,7 @@ import {Navigate, Outlet} from "react-router-dom";
 
 function PrivateRoutes() {
     let auth = false;
-    return auth ? (<Outlet/>) : (<Navigate to={"/"} />);
+    return auth ? (<Outlet/>) : (<Navigate to={"/login"} />);
 }
 
 export default PrivateRoutes;
