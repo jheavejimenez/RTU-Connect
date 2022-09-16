2e67f3d211d82eb05481ce207d67996810dd3223
jheave <jimenezjheave123@gmail.com>
2022-09-16 22:14:13 +0800

    feat: private routes

---
+++ frontend/src/utils/PrivateRoutes.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {Navigate, Outlet} from "react-router-dom";
+
+function PrivateRoutes() {
+    let auth = false;
+    return auth ? (<Outlet/>) : (<Navigate to={"/login"} />);
+}
+
+export default PrivateRoutes;
