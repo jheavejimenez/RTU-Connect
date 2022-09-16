b523ad883098ac65dca7127d2d0a9d79e993e181
jheave <jimenezjheave123@gmail.com>
2022-09-16 22:16:04 +0800

    feat: private routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,17 @@ import "./index.css"
 import Home from "./pages/Home";
 import {Route, Routes} from "react-router-dom";
 import Login from "./pages/Login";
+import PrivateRoutes from "./utils/PrivateRoutes";
 
 
 function App() {
     return (
         <div>
             <Routes>
+                <Route element={<PrivateRoutes />}>
+                    <Route path="/home" element={<Home />} />
+                </Route>
                 <Route path="/" element={<Login/>}/>
-                <Route path="/home" element={<Home/>}/>
             </Routes>
         </div>
     );
--- frontend/src/utils/PrivateRoutes.jsx
+++ frontend/src/utils/PrivateRoutes.jsx
@@ -3,7 +3,7 @@ import {Navigate, Outlet} from "react-router-dom";
 
 function PrivateRoutes() {
     let auth = false;
-    return auth ? (<Outlet/>) : (<Navigate to={"/login"} />);
+    return auth ? (<Outlet/>) : (<Navigate to={"/"} />);
 }
 
 export default PrivateRoutes;
