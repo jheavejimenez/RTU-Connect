c17b6176492bd2b3bb6b72415909f91629b41d63
jheave <jimenezjheave123@gmail.com>
2022-09-16 22:55:46 +0800

    add dependencies

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,69 @@
 import {useEffect, useState} from "react";
 import {Web3AuthCore} from '@web3auth/core'
-import {
-    WALLET_ADAPTERS,
-    CHAIN_NAMESPACES,
-    SafeEventEmitterProvider,
-} from '@web3auth/base'
+import {CHAIN_NAMESPACES, WALLET_ADAPTERS,} from '@web3auth/base'
 import {OpenloginAdapter} from '@web3auth/openlogin-adapter'
 import Button from "../components/Button/Button";
 
+const clientId = process.env.REACT_APP_CLIENT_ID;
+
 function Login() {
+    const [web3auth, setWeb3auth] = useState(null)
+    const [provider, setProvider] = useState(null)
+
+    useEffect(() => {
+        const init = async () => {
+            try {
+                const web3auth = new Web3AuthCore({
+                    clientId,
+                    chainConfig: {
+                        chainNamespace: CHAIN_NAMESPACES.EIP155,
+                        chainId: '0x3',
+                    },
+                })
+
+                const openloginAdapter = new OpenloginAdapter({
+                    adapterSettings: {
+                        network: 'testnet',
+                        uxMode: 'popup',
+                        loginConfig: {
+                            google: {
+                                name: 'Custom Google Auth Login',
+                                verifier: 'web3auth-core-google',
+                                typeOfLogin: 'google',
+                                clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID, //use app client id from google
+                            },
+                        },
+                    },
+                })
+                web3auth.configureAdapter(openloginAdapter)
+                setWeb3auth(web3auth)
+
+                await web3auth.init()
+                if (web3auth.provider) {
+                    setProvider(web3auth.provider);
+                }
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        init()
+    }, [])
+
+    const handleLogin = async () => {
+        if (!web3auth) {
+            console.log('web3auth not initialized yet')
+            return
+        }
+        const web3authProvider = await web3auth.connectTo(
+            WALLET_ADAPTERS.OPENLOGIN,
+            {
+                loginProvider: 'google',
+            },
+        )
+        setProvider(web3authProvider)
+    }
+
     return (
         <div className={"container flex items-center justify-center mx-auto h-screen"}>
             <div className={"flex w-full justify-center px-6"}>
@@ -18,7 +73,7 @@ function Login() {
                             <img className={"mx-auto"} src={require("../images/RTULogo.png")}/>
                         </div>
                         <div className={"text-center"}>
-                            <Button text="Login" path={"/"}/>
+                           <button className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"} onClick={handleLogin}>Login</button>
                         </div>
                         <hr className={"my-6"}/>
                         <div className={"text-center m-4"}>
