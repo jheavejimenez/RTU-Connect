03024778a0499c96de76f94540a9f53b6b323ea2
jheave <jimenezjheave123@gmail.com>
2022-09-16 21:40:45 +0800

    refractor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import "./index.css"
 import Home from "./pages/Home";
 import {Route, Routes} from "react-router-dom";
 import Login from "./pages/Login";
-import Post from "./components/Post/Post";
 
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
                 <Route path="/" element={<Login/>}/>
                 <Route path="/home" element={<Home/>}/>
             </Routes>
-            <Post/>
         </div>
     );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "../index.css"
 import NavBar from "../components/NavBar/NavBar";
 import ComposePost from "../components/ComposePost/ComposePost";
+import Post from "../components/Post/Post";
 
 function Home() {
     return (
         <div className="Home">
             <NavBar/>
             <ComposePost/>
+            <Post/>
         </div>
     )
 }
