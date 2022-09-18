7e9c99a27adf461a8564e84f35f28c2cc469cb95
jheave <jimenezjheave123@gmail.com>
2022-09-19 02:02:30 +0800

    feat: redirect login

--- frontend/src/utils/PrivateRoutes.jsx
+++ frontend/src/utils/PrivateRoutes.jsx
@@ -5,6 +5,7 @@ import { useMoralis } from "react-moralis";
 function PrivateRoutes() {
     const location = useLocation();
     const { isAuthenticated } = useMoralis();
+    console.log("isAuthenticated", isAuthenticated);
     return isAuthenticated ? (<Outlet />) : (<Navigate to={"/login"} replace state={{ from: location }} />);
 }
 
