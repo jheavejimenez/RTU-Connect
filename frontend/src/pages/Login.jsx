28692e1b6593c5b9ee51438073bf2763b063bf31
jheave <jimenezjheave123@gmail.com>
2022-09-19 01:52:52 +0800

    feat: more sign in options

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,112 +1,118 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Web3AuthCore } from "@web3auth/core";
-import { CHAIN_NAMESPACES, WALLET_ADAPTERS } from "@web3auth/base";
-import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useMoralis } from "react-moralis";
 import ButtonFunctionCall from "../components/Button/ButtonFunctionCall";
-import ProviderContext from "../context/ProviderContext";
 
-const clientId = process.env.REACT_APP_WEB3_AUTH_CLIENT_ID;
-const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+const webAuthClientId = process.env.REACT_APP_WEB3_AUTH_CLIENT_ID;
 
 function Login() {
-    const [web3auth, setWeb3auth] = useState(null);
-    const [provider, setProvider] = useState(null);
-    const { user, setUser } = useContext(ProviderContext);
     const navigate = useNavigate();
     const location = useLocation();
+    const {
+        authenticate, authError, isAuthenticating, Moralis,
+    } = useMoralis();
 
-    useEffect(() => {
-        const init = async () => {
-            try {
-                const web3auth = new Web3AuthCore({
-                    clientId,
-                    chainConfig: {
-                        chainNamespace: CHAIN_NAMESPACES.EIP155,
-                        chainId: "0x13881", // hex of 80001, polygon testnet
-                        // rpcTarget: "https://rpc.ankr.com/polygon_mumbai",
-                        // // Avoid using public rpcTarget in production.
-                        // // Use services like Infura, Quicknode etc
-                        // displayName: "Polygon Mainnet",
-                        // blockExplorer: "https://mumbai.polygonscan.com/",
-                        // ticker: "MATIC",
-                        // tickerName: "Matic",
-                    },
-                });
-
-                const openloginAdapter = new OpenloginAdapter({
-                    adapterSettings: {
-                        network: "testnet",
-                        uxMode: "popup",
-                        whiteLabel: {
-                            name: "RTU Connect",
-                            logoLight: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
-                            defaultLanguage: "en",
-                        },
-                        loginConfig: {
-                            google: {
-                                name: "RTU Connect",
-                                verifier: "rtu-connect",
-                                typeOfLogin: "google",
-                                clientId: googleClientId, // use app client id from Google
-                            },
-                        },
-                    },
-                });
-                web3auth.configureAdapter(openloginAdapter);
-                setWeb3auth(web3auth);
-
-                await web3auth.init();
-                if (web3auth.provider) {
-                    setProvider(web3auth.provider);
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        init();
-    }, []);
+    // useEffect(() => {
+    //     const init = async () => {
+    //         try {
+    //             const web3auth = new Web3AuthCore({
+    //                 clientId,
+    //                 chainConfig: {
+    //                     chainNamespace: CHAIN_NAMESPACES.EIP155,
+    //                     chainId: "0x13881", // hex of 80001, polygon testnet
+    //                     // rpcTarget: "https://rpc.ankr.com/polygon_mumbai",
+    //                     // // Avoid using public rpcTarget in production.
+    //                     // // Use services like Infura, Quicknode etc
+    //                     // displayName: "Polygon Mainnet",
+    //                     // blockExplorer: "https://mumbai.polygonscan.com/",
+    //                     // ticker: "MATIC",
+    //                     // tickerName: "Matic",
+    //                 },
+    //             });
+    //
+    //             const openloginAdapter = new OpenloginAdapter({
+    //                 adapterSettings: {
+    //                     network: "testnet",
+    //                     uxMode: "popup",
+    //                     whiteLabel: {
+    //                         name: "RTU Connect",
+    //                         logoLight: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+    //                         defaultLanguage: "en",
+    //                     },
+    //                     loginConfig: {
+    //                         google: {
+    //                             name: "RTU Connect",
+    //                             verifier: "rtu-connect",
+    //                             typeOfLogin: "google",
+    //                             clientId: googleClientId, // use app client id from Google
+    //                         },
+    //                     },
+    //                 },
+    //             });
+    //             web3auth.configureAdapter(openloginAdapter);
+    //             setWeb3auth(web3auth);
+    //
+    //             await web3auth.init();
+    //             if (web3auth.provider) {
+    //                 setProvider(web3auth.provider);
+    //             }
+    //         } catch (error) {
+    //             console.error(error);
+    //         }
+    //     };
+    //
+    //     init();
+    // }, []);
+    //
+    // const handleLogin = async () => {
+    //     if (!web3auth) {
+    //         console.log("web3auth not initialized yet");
+    //         return;
+    //     }
+    //     const web3authProvider = await web3auth.connectTo(
+    //         WALLET_ADAPTERS.OPENLOGIN,
+    //         {
+    //             loginProvider: "google",
+    //         },
+    //     );
+    //     setProvider(web3authProvider);
+    //
+    //     // this code must be change  because it is not secure
+    //     if (user.loggedIn) return;
+    //     setUser({ loggedIn: true });
+    //
+    //     // check if provider is set and  location.state.from is set
+    //     if (location.state?.from) {
+    //         navigate(location.state.from);
+    //     }
+    // };
+    // const logout = async () => {
+    //     if (!web3auth) {
+    //         console.log("web3auth not initialized yet");
+    //         return;
+    //     }
+    //     await web3auth.logout();
+    //     setProvider(null);
+    // };
+    // const getUserInfo = async () => {
+    //     if (!web3auth) {
+    //         console.log("web3auth not initialized yet");
+    //         return;
+    //     }
+    //     const user = await web3auth.getUserInfo();
+    //     console.log(user);
+    // };
 
     const handleLogin = async () => {
-        if (!web3auth) {
-            console.log("web3auth not initialized yet");
-            return;
-        }
-        const web3authProvider = await web3auth.connectTo(
-            WALLET_ADAPTERS.OPENLOGIN,
-            {
-                loginProvider: "google",
-            },
-        );
-        setProvider(web3authProvider);
-
-        // this code must be change  because it is not secure
-        if (user.loggedIn) return;
-        setUser({ loggedIn: true });
-
-        // check if provider is set and  location.state.from is set
-        if (location.state?.from) {
-            navigate(location.state.from);
-        }
+        await authenticate({
+            provider: "web3Auth",
+            clientId: webAuthClientId,
+            chainId: Moralis.Chains.POLYGON_MUMBAI,
+            appLogo: "https://images.web3auth.io/web3auth-logo-w.svg",
+            theme: "light",
+            loginMethodsOrder: ["google"],
+        });
     };
-    const logout = async () => {
-        if (!web3auth) {
-            console.log("web3auth not initialized yet");
-            return;
-        }
-        await web3auth.logout();
-        setProvider(null);
-    };
-    const getUserInfo = async () => {
-        if (!web3auth) {
-            console.log("web3auth not initialized yet");
-            return;
-        }
-        const user = await web3auth.getUserInfo();
-        console.log(user);
-    };
-
     return (
         <div className={"container flex items-center justify-center mx-auto h-screen"}>
             <div className={"flex w-full justify-center px-6"}>
@@ -119,19 +125,16 @@ function Login() {
                                 alt={"logo of RTU"}
                             />
                         </div>
-                        <div className={"text-center"}>
-                            {!provider ? (
+                        <div className={"text-center flex items-center justify-center"}>
+                            {isAuthenticating ? (
+                                <div className={"w-10 h-10 border-t-2 border-b-2 border-blue-500 rounded-full animate-spin"} />
+                            ) : (
                                 <ButtonFunctionCall
                                     text={"Login"}
                                     func={handleLogin}
                                 />
-                            ) : (
-                                //  in the future this must be changed
-                                <>
-                                    <ButtonFunctionCall text={"logout"} func={logout} />
-                                    <ButtonFunctionCall text={"user"} func={getUserInfo} />
-                                </>
                             )}
+                            
                         </div>
                         <hr className={"my-6"} />
                         <div className={"text-center m-4"}>
--- frontend/src/utils/PrivateRoutes.jsx
+++ frontend/src/utils/PrivateRoutes.jsx
@@ -2,14 +2,14 @@ import React, { useContext } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import ProviderContext from "../context/ProviderContext";
 
-const useAuth = () => {
-    const { user } = useContext(ProviderContext);
-    return user && user.loggedIn;
-};
+// const useAuth = () => {
+//     const { user } = useContext(ProviderContext);
+//     return user && user.loggedIn;
+// };
 
 function PrivateRoutes() {
     const location = useLocation();
-    const isAuth = useAuth();
+    const isAuth = true;
     return isAuth ? (<Outlet />) : (<Navigate to={"/login"} replace state={{ from: location }} />);
 }
 
