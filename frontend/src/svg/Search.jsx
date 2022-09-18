f7030a547d15fec02f5c3a5a05d3dd4d4c19e263
jheave <jimenezjheave123@gmail.com>
2022-09-18 19:26:00 +0800

    refractor

--- frontend/src/svg/Search.jsx
+++ frontend/src/svg/Search.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 
-const SVGSearch = () => (
-    <svg
-        className={"w-5 text-gray-600 h-5 cursor-pointer"}
-        fill={"none"}
-        strokeLinecap={"round"}
-        strokeLinejoin={"round"}
-        strokeWidth={"2"}
-        stroke={"currentColor"}
-        viewBox={"0 0 24 24"}>
-        <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-    </svg>
-)
+function SVGSearch() {
+    return (
+        <svg
+            className={"w-5 text-gray-600 h-5 cursor-pointer"}
+            fill={"none"}
+            strokeLinecap={"round"}
+            strokeLinejoin={"round"}
+            strokeWidth={"2"}
+            stroke={"currentColor"}
+            viewBox={"0 0 24 24"}
+        >
+            <path d={"M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"} />
+        </svg>
+    );
+}
 
 export default SVGSearch;
