9d015cc97547756382d61168a861cd4084c09362
jheave <jimenezjheave123@gmail.com>
2022-09-27 23:44:48 +0800

    fix ts variant config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
+import CreateProfile from "./pages/CreateProfile";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Route path={"/"} element={<Home />} />
                 <Route path={"/profile"} element={<Profile />} />
                 <Route path={"/login"} element={<Login />} />
+                <Route path={"/new/profile"} element={<CreateProfile />} />
             </Routes>
         </>
     );
--- frontend/src/pages/CreateProfile.jsx
+++ frontend/src/pages/CreateProfile.jsx
@@ -1,5 +1,6 @@
-import React, { createRef, useEffect, useState } from "react";
-import { gql, useMutation } from "@apollo/client";
+import React, { useState, useEffect, createRef } from "react";
+import { gql, useQuery, useMutation } from "@apollo/client";
+import ButtonFunctionCall from "../components/Button/ButtonFunctionCall";
 
 const CREATE_PROFILE = gql`
     mutation ($request: CreateProfileRequest!) {
@@ -25,111 +26,43 @@ const MODULE_APPROVAL_DATA = gql`
     }
 `;
 
-function NewProfile({ profile = {}, wallet }) {
-    const [newProfile, setNewProfile] = useState({ ...profile });
+function NewProfile() {
     const [createProfile, createProfileData] = useMutation(CREATE_PROFILE);
     const handleRef = createRef();
-    const costRef = createRef();
-
-    // const bioRef = createRef()
-
-    const handleCreate = async () => {
-        const handle = handleRef.current.value.replace("@", "");
-        if (!handle) {
-            console.log("no handle");
-            return;
-        }
-        const cost = costRef.current.value;
-        if (!cost) {
-            console.log("no cost");
-            return;
-        }
-
-        const profileRequest = {
-            handle,
-        };
-
-        // const bio = bioRef.current.value
-        await createProfile({
-            variables: {
-                request: profileRequest,
-            },
-        });
-    };
-
-    useEffect(() => {
-        if (!createProfileData.data) return;
-
-        console.log(createProfileData.data);
-    }, [createProfileData.data]);
-
-    const handleHandle = (e) => {
-        if (e.target.value[0] !== "@") {
-            e.target.value = `@${e.target.value}`;
-        }
-        if (e.target.value.length === 1) {
-            e.target.value = "";
-        }
-    };
 
     return (
         <div className={"relative flex flex-col justify-center min-h-screen overflow-hidden "}>
             <div
-                className={"w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring-2 ring-indigo-600 lg:max-w-xl"}
+                className={"w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl"}
             >
-                <h1 className={"text-3xl font-semibold text-center text-indigo-700 underline uppercase decoration-wavy"}>
-                    {"Contact Form"}
+                <h1 className={"text-3xl font-semibold text-center text-gray-600"}>
+                    {"Create Profile"}
                 </h1>
-                <form className={"mt-6"}>
-                    <div className={"mb-2"}>
-                        <span className={"text-gray-700"}>{"Your name"}</span>
-                        <input
-                            type={"text"}
-                            name={"name"}
-                            className={"w-fullblock px-16 py-2 mt-2 border-gray-300 rounded-md"
-                                    + "shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200"
-                                    + "focus:ring-opacity-50"}
-                            placeholder={"John cooks"}
-                        />
+                <form className={"mt-5"}>
+                    <div className={"flex flex-col space-y-4"}>
+                        <div className={"flex flex-col space-y-1"}>
+                            <label
+                                htmlFor={"name"}
+                                className={"text-sm font-semibold text-gray-600"}
+                            >
+                                {"Enter yor profile handle"}
+                            </label>
+                            <input
+                                id={"name"}
+                                type={"text"}
+                                placeholder={"@john_doe"}
+                                autoComplete={"off"}
+                                className={"w-full px-4 py-2 text-gray-700 bg-gray-200"
+                                    + " rounded-lg focus:outline-none focus:bg-gray-100"}
+                            />
+                        </div>
+                        <div className={"flex flex-col space-y-1"}>
+                            <ButtonFunctionCall text={"Create Profile"} func={() => {}} />
+                        </div>
                     </div>
-                    <div className={"mb-2"}>
-                        <span className={"text-gray-700"}>{"Email address"}</span>
-                        <input
-                            name={"email"}
-                            type={"email"}
-                            className={"block w-full mt-2 px-16 py-2 border-gray-300 rounded-md "
-                                    + "shadow-sm focus:border-indigo-300 focus:ring "
-                                    + "focus:ring-indigo-200 focus:ring-opacity-50"}
-                            placeholder={"john.cooks@example.com"}
-                            required
-                        />
-                    </div>
-                    <div className={"mb-2"}>
-                        <span className={"text-gray-700"}>{"Message"}</span>
-                        <textarea
-                            name={"message"}
-                            className={"block w-full mt-2 px-16 py-8 border-gray-300 "
-                                    + "rounded-md shadow-sm focus:border-indigo-300 focus:ring "
-                                    + "focus:ring-indigo-200 focus:ring-opacity-50"}
-                            rows={"5"}
-                        />
-                    </div>
-
-                    <div className={"mb-6"}>
-                        <button
-                            type={"submit"}
-                            className={"h-10 px-5 text-indigo-100 bg-indigo-700 "
-                                + "rounded-lg transition-colors duration-150 "
-                                + "focus:shadow-outline hover:bg-indigo-800"}
-                        >
-                            {"Contact Us"}
-                        </button>
-                    </div>
-                    <div />
                 </form>
             </div>
         </div>
     );
 }
-
 export default NewProfile;
