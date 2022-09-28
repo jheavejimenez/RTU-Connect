b565af057513a9d04dc727eb64810874edd60739
jheave <jimenezjheave123@gmail.com>
2022-09-29 01:30:41 +0800

    fix log in

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,11 @@ import { sequence } from "0xsequence";
 import Web3Modal from "@0xsequence/web3modal";
 import WalletConnect from "@walletconnect/web3-provider";
 import { ethers } from "ethers";
+import { useLazyQuery, useMutation } from "@apollo/client";
 import ButtonFunctionCall from "../components/Button/ButtonFunctionCall";
+import { AUTHENTICATION, GET_CHALLENGE } from "../GraphQL/mutations";
+import lensHub from "../utils/lensHub.json";
+import { ADDRESS } from "../utils/constants";
 
 // Configure  wallet
 let providerOptions = {
@@ -33,8 +37,14 @@ const web3Modal = new Web3Modal({
     cacheProvider: true,
 });
 
-function Login() {
+function Login({
+    wallet, setWallet, auth, setLensHub, 
+}) {
     const [provider, setProvider] = useState(null);
+    const [authToken, setAuthToken] = auth;
+    const [getChallenge, challengeData] = useLazyQuery(GET_CHALLENGE);
+    const [mutateAuth, authData] = useMutation(AUTHENTICATION);
+
     const connectWallet = async () => {
         const wallet = await web3Modal.connect();
 
@@ -43,6 +53,29 @@ function Login() {
         if (wallet.sequence) {
             (provider).sequence = wallet.sequence;
         }
+
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+
+        const contract = new ethers.Contract(ADDRESS.lensHub, lensHub, signer);
+        console.log({ contract });
+        setLensHub(contract);
+        setWallet({ ...wallet, signer, address });
+
+        if (authToken) {
+            console.log("login: already logged in");
+            return;
+        }
+
+        console.log("login: address", address);
+
+        await getChallenge({
+            variables: {
+                request: {
+                    address,
+                },
+            },
+        });
 
         setProvider(provider);
     };
@@ -59,6 +92,38 @@ function Login() {
         }
         connectWallet();
     };
+    useEffect(() => {
+        if (!challengeData.data) return;
+
+        const handleSign = async () => {
+            const signature = await wallet.signer.signMessage(challengeData.data.challenge.text);
+            mutateAuth({
+                variables: {
+                    request: {
+                        address: wallet.address,
+                        signature,
+                    },
+                },
+            });
+        };
+
+        handleSign();
+    }, [challengeData.data]);
+
+    useEffect(() => {
+        if (!authData.data) return;
+
+        // window.authToken = authData.data.authenticate.accessToken
+        window.sessionStorage.setItem("lensToken", authData.data.authenticate.accessToken);
+
+        setAuthToken(true);
+    }, [authData.data]);
+
+    useEffect(() => {
+        if (window.sessionStorage.getItem("lensToken")) {
+            setAuthToken(true);
+        }
+    }, []);
 
     const disconnectWeb3Modal = async () => {
         web3Modal.clearCachedProvider();
