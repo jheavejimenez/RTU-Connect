7100fd8733d7b5262c73510991774db69499cdf4
jheave <jimenezjheave123@gmail.com>
2022-09-30 18:21:17 +0800

    remove moralis

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { MoralisProvider } from "react-moralis";
 import App from "./App";
 import ApolloProvider from "./context/ProviderContext";
 import { AuthProvider } from "./hooks/useAuth";
@@ -9,17 +8,12 @@ import { AuthProvider } from "./hooks/useAuth";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <MoralisProvider
-            serverUrl={process.env.REACT_APP_MORALIS_SERVER_URL}
-            appId={process.env.REACT_APP_MORALIS_APP_ID}
-        >
-            <BrowserRouter>
-                <AuthProvider>
-                    <ApolloProvider>
-                        <App />
-                    </ApolloProvider>
-                </AuthProvider>
-            </BrowserRouter>
-        </MoralisProvider>
+        <BrowserRouter>
+            <AuthProvider>
+                <ApolloProvider>
+                    <App />
+                </ApolloProvider>
+            </AuthProvider>
+        </BrowserRouter>
     </React.StrictMode>,
 );
