d94d402d3c8f6bd1ed768ca85775d46ddfa9d444
jheave <jimenezjheave123@gmail.com>
2022-10-04 23:19:48 +0800

    fix url

--- frontend/src/utils/pinataAPICall.js
+++ frontend/src/utils/pinataAPICall.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // eslint-disable-next-line import/prefer-default-export
 export const submarine = async (data) => {
-    const res = await axios.get("https://api.pinata.cloud/data/testAuthentication", data, {
+    const res = await axios.get("https://api.pinata.cloud/pinning/pinJSONToIPFS", data, {
         headers: {
             "contentType": "application/json",
             "Authorization": `Bearer ${process.env.REACT_APP_PINATA_JWT}`,
