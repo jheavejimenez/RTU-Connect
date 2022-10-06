cc0235f7e1e9da2eabc6a1662b4abde9f8759b90
jheave <jimenezjheave123@gmail.com>
2022-10-06 19:35:45 +0800

    add helpers

---
+++ frontend/src/utils/helpers.js
@@ -0,0 +1,31 @@
+import { ethers, utils } from "ethers";
+import omitDeep from "omit-deep";
+
+export function getSigner() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    return provider.getSigner();
+}
+
+export function signedTypeData(domain, types, value) {
+    const signer = getSigner();
+    // eslint-disable-next-line no-underscore-dangle
+    return signer._signTypedData(
+        omitDeep(domain, "__typename"),
+        omitDeep(types, "__typename"),
+        omitDeep(value, "__typename"),
+    );
+}
+
+export function splitSignature(signature) {
+    return utils.splitSignature(signature);
+}
+export function generateRandomColor() {
+    const maxVal = 0xFFFFFF; // 16777215
+    let randomNumber = Math.random() * maxVal;
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    const randColor = randomNumber.padStart(6, 0);
+    return `#${randColor.toUpperCase()}`;
+}
+
+export const fixURL = (url) => url?.replace("ipfs://", "https://lens.infura-ipfs.io/ipfs/");
