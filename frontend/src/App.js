4741166df2c124cee66fc35006361552591c3054
jheave <jimenezjheave123@gmail.com>
2022-10-09 18:19:48 +0800

    feat: get comment feed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,7 +43,7 @@ function App() {
                     )}
                 />
                 <Route
-                    path={"/comments/:id"}
+                    path={"/comments/:publicationId"}
                     element={(
                         <ProtectedRoute>
                             <Comment />
--- frontend/src/pages/Comment.jsx
+++ frontend/src/pages/Comment.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_COMMENT_FEED } from "../graphQL/queries";
 
 function Comment() {
+    const { publicationId } = useParams();
+    const [comments, setComments] = useState([]);
+
+    const { data } = useQuery(GET_COMMENT_FEED, {
+        variables: {
+            request: {
+                "commentsOf": publicationId,
+                "limit": 10,
+            },
+        },
+    });
+
+    useEffect(() => {
+        if (!data) return;
+        setComments(data.publications.items);
+    }, [data]);
+    
     return (
         <>
-            <h1>{"Comment"}</h1>
+            <h1>{"hey"}</h1>
         </>
     );
 }
