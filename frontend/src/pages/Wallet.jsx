13308eb44e37204c613ce87e54885cb8d6a7ebf5
jheave <jimenezjheave123@gmail.com>
2022-12-07 23:04:51 +0800

    feat: loading in signing

--- frontend/src/pages/Wallet.jsx
+++ frontend/src/pages/Wallet.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import toast from "react-hot-toast";
 import Web3Modal from "@0xsequence/web3modal";
 import { ethers } from "ethers";
@@ -201,6 +201,52 @@ function Wallet({
             console.log(error);
         }
     }
+
+    function lensLogin() {
+        if (challengeLoading || authenticateLoading || profileLoading) {
+            // button loading using tailwind
+            return (
+                <button
+                    type={"button"}
+                    className={"flex items-center justify-center w-60 px-4 py-2"
+                        + " font-bold text-white bg-blue-500 rounded-full"}
+                >
+                    <svg
+                        className={"w-5 h-5 mr-3 -ml-1 text-white animate-spin"}
+                        xmlns={"http://www.w3.org/2000/svg"}
+                        fill={"none"}
+                        viewBox={"0 0 24 24"}
+                    >
+                        <circle
+                            className={"opacity-25"}
+                            cx={"12"}
+                            cy={"12"}
+                            r={"10"}
+                            stroke={"currentColor"}
+                            strokeWidth={"4"}
+                        />
+                        <path
+                            className={"opacity-75"}
+                            fill={"currentColor"}
+                            d={"M4 12a8 8 0 018-8v8H4z"}
+                        />
+                    </svg>
+                    <span>{"Signing in..."}</span>
+                </button>
+            );
+        }
+        return (
+            <button
+                type={"button"}
+                className={"w-60 px-4 py-2 font-bold text-white bg-blue-500 rounded-full "
+                    + "hover:bg-blue-700 focus:outline-none focus:shadow-outline"}
+                onClick={handleLogin}
+            >
+                {"sign in to RTU Connect"}
+            </button>
+        );
+    }
+
     return (
         <div className={"container flex items-center justify-center mx-auto h-screen"}>
             <div className={"flex w-full justify-center px-6"}>
@@ -223,11 +269,7 @@ function Wallet({
                                     />
                                 )
                                 : (
-                                    <ButtonFunctionCall
-                                        func={handleLogin}
-                                        text={"Sign In to RTU-Connect"}
-                                    />
-
+                                    lensLogin()
                                 )}
                         </div>
                         <hr className={"my-6"} />
@@ -250,6 +292,8 @@ function Wallet({
                     />
                 </div>
             </div>
+            {(challengeError || authenticateError || profileError)
+                && toast.error("Error logging in. Please refresh the browser and try again")}
         </div>
     );
 }
