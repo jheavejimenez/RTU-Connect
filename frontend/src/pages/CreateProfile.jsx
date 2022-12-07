ebab9459b10ccfb707305b48d40f68330ee626b0
jheave <jimenezjheave123@gmail.com>
2022-12-07 20:09:51 +0800

    feat: loading state

--- frontend/src/pages/CreateProfile.jsx
+++ frontend/src/pages/CreateProfile.jsx
@@ -8,7 +8,6 @@ function CreateHandle() {
     const [createProfile, createProfileData, { loading }] = useMutation(CREATE_PROFILE);
     const handleRef = createRef();
     const navigate = useNavigate();
-
     const handleCreate = async (e) => {
         e.preventDefault();
         const handle = handleRef.current.value.replace("@", "");
@@ -44,38 +43,47 @@ function CreateHandle() {
 
     return (
         <div className={"relative flex flex-col justify-center min-h-screen overflow-hidden "}>
-            <div
-                className={"w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl"}
-            >
-                <h1 className={"text-3xl font-semibold text-center text-gray-600"}>
-                    {"Create Profile"}
-                </h1>
-                <form className={"mt-5"}>
-                    <div className={"flex flex-col space-y-4"}>
-                        <div className={"flex flex-col space-y-1"}>
-                            <label
-                                htmlFor={"name"}
-                                className={"text-sm font-semibold text-gray-600"}
-                            >
-                                {"Enter your profile username"}
-                            </label>
-                            <input
-                                id={"name"}
-                                type={"text"}
-                                placeholder={"@username"}
-                                ref={handleRef}
-                                onChange={inputHandleValue}
-                                autoComplete={"off"}
-                                className={"w-full px-4 py-2 text-gray-700 bg-gray-200"
-                                    + " rounded-lg focus:outline-none focus:bg-gray-100"}
-                            />
-                        </div>
-                        <div className={"flex flex-col space-y-1"}>
-                            <ButtonFunctionCall text={"Create Profile"} func={handleCreate} />
-                        </div>
+            {loading ? (
+            // loading animation
+                <div className={"flex justify-center"}>
+                    <div className={"flex flex-col items-center justify-center"}>
+                        <span className={"animate-pulse font-bold text-gray-800"}>{"Creating profile..."}</span>
                     </div>
-                </form>
-            </div>
+                </div>
+            ) : (
+                <div
+                    className={"w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl"}
+                >
+                    <h1 className={"text-3xl font-semibold text-center text-gray-600"}>
+                        {"Create Profile"}
+                    </h1>
+                    <form className={"mt-5"}>
+                        <div className={"flex flex-col space-y-4"}>
+                            <div className={"flex flex-col space-y-1"}>
+                                <label
+                                    htmlFor={"name"}
+                                    className={"text-sm font-semibold text-gray-600"}
+                                >
+                                    {"Enter your profile username"}
+                                </label>
+                                <input
+                                    id={"name"}
+                                    type={"text"}
+                                    placeholder={"@username"}
+                                    ref={handleRef}
+                                    onChange={inputHandleValue}
+                                    autoComplete={"off"}
+                                    className={"w-full px-4 py-2 text-gray-700 bg-gray-200"
+                                    + " rounded-lg focus:outline-none focus:bg-gray-100"}
+                                />
+                            </div>
+                            <div className={"flex flex-col space-y-1"}>
+                                <ButtonFunctionCall text={"Create Profile"} func={handleCreate} />
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            )}
         </div>
     );
 }
